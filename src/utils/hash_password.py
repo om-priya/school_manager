d8b37f82a5763ebff8a3d1bdc71d0a62c4553db8
Om Priya <ompriya18153789@gmail.com>
2023-11-17 16:43:27 +0530

    reset main branch

--- src/utils/hash_password.py
+++
@@ -1,8 +0,0 @@
-""" To hash the password using hashlib """
-import hashlib
-
-
-def hash_password(password):
-    """This function is responsible for hashing password"""
-    hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
-    return hashed_password
