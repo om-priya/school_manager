80589579a39731354bf154e5011d1626e821a7fc
Om Priya <ompriya18153789@gmail.com>
2024-02-06 15:31:13 +0530

    Added logging to controllers

--- src/controllers/principal_controller.py
+++ src/controllers/principal_controller.py
@@ -4,6 +4,7 @@ from flask_smorest import abort
 from utils.custom_error import DataNotFound, AlreadyPresent
 from models.response_format import SuccessResponse, ErrorResponse
 from handlers.principal_handler import PrincipalHandler
+from helper.helper_function import get_request_id
 from config.display_menu import PromptMessage
 import logging
 
@@ -13,11 +14,18 @@ logger = logging.getLogger(__name__)
 class PrincipalController:
     def get_all_principals(self):
         try:
+            logger.info(
+                f"{get_request_id()} Calling handler for getting all principals"
+            )
             res_data = PrincipalHandler().get_all_principal()
+            logger.info(f"{get_request_id()} formatting response after getting details")
             return SuccessResponse(
                 200, PromptMessage.LIST_ENTRY.format("Principals"), res_data
             ).get_json()
         except DataNotFound:
+            logger.info(
+                f"{get_request_id()} formatting response for no principal found"
+            )
             return abort(
                 404,
                 message=ErrorResponse(
@@ -27,11 +35,18 @@ class PrincipalController:
 
     def get_single_principal(self, principal_id):
         try:
+            logger.info(
+                f"{get_request_id()} Calling handler for getting principal by id"
+            )
             res_data = PrincipalHandler().get_principal_by_id(principal_id)
+            logger.info(f"{get_request_id()} formatting response after getting details")
             return SuccessResponse(
                 200, PromptMessage.LIST_ENTRY.format("Principal"), res_data
             ).get_json()
         except DataNotFound:
+            logger.info(
+                f"{get_request_id()} formatting response for no principal found"
+            )
             return abort(
                 404,
                 message=ErrorResponse(
@@ -41,11 +56,18 @@ class PrincipalController:
 
     def approve_principal(self, principal_id):
         try:
+            logger.info(f"{get_request_id()} Calling handler for approving principal")
             PrincipalHandler().approve_principal(principal_id)
+            logger.info(
+                f"{get_request_id()} formatting response after successfull approval"
+            )
             return SuccessResponse(
                 200, PromptMessage.APPROVE_SUCCESS.format("Principal")
             ).get_json()
         except DataNotFound:
+            logger.info(
+                f"{get_request_id()} formatting response for no principal found"
+            )
             return abort(
                 404,
                 message=ErrorResponse(
@@ -53,6 +75,7 @@ class PrincipalController:
                 ).get_json(),
             )
         except AlreadyPresent:
+            logger.info(f"{get_request_id()} Tried to add more than one principal")
             return abort(
                 409,
                 message=ErrorResponse(
@@ -62,11 +85,18 @@ class PrincipalController:
 
     def delete_principal(self, principal_id):
         try:
+            logger.info(f"{get_request_id()} Calling handler for deleting principal")
             PrincipalHandler().delete_principal(principal_id)
+            logger.info(
+                f"{get_request_id()} fromating response after deleting principal"
+            )
             return SuccessResponse(
                 200, PromptMessage.SUCCESS_ACTION.format("Principal Deleted")
             ).get_json()
         except DataNotFound:
+            logger.info(
+                f"{get_request_id()} formatting response for no principal found to delete with id {principal_id}"
+            )
             return abort(
                 404,
                 message=ErrorResponse(
--- src/controllers/teacher_controller.py
+++ src/controllers/teacher_controller.py
@@ -7,6 +7,7 @@ from utils.custom_error import DataNotFound, FailedAction
 from models.response_format import SuccessResponse, ErrorResponse
 from handlers.teacher_handler import TeacherHandler
 from config.display_menu import PromptMessage
+from helper.helper_function import get_request_id
 
 logger = logging.getLogger(__name__)
 
@@ -14,11 +15,14 @@ logger = logging.getLogger(__name__)
 class TeacherController:
     def get_all_teacher(self):
         try:
+            logger.info(f"{get_request_id()} Calling handler for getting all teachers")
             res_data = TeacherHandler().get_all_teacher()
+            logger.info(f"{get_request_id()} formatting response after getting details")
             return SuccessResponse(
                 200, PromptMessage.LIST_ENTRY.format("Teachers"), res_data
             ).get_json()
         except DataNotFound:
+            logger.info(f"{get_request_id()} formatting response for no teacher found")
             return abort(
                 404,
                 message=ErrorResponse(
@@ -28,11 +32,14 @@ class TeacherController:
 
     def get_single_teacher(self, teacher_id):
         try:
+            logger.info(f"{get_request_id()} Calling handler for getting teacher by id")
             res_data = TeacherHandler().get_teacher_by_id(teacher_id)
+            logger.info(f"{get_request_id()} formatting response after getting details")
             return SuccessResponse(
                 200, PromptMessage.LIST_ENTRY.format("Teacher"), res_data
             ).get_json()
         except DataNotFound:
+            logger.info(f"{get_request_id()} formatting response for no teacher found")
             return abort(
                 404,
                 message=ErrorResponse(
@@ -42,11 +49,16 @@ class TeacherController:
 
     def approve_teacher(self, teacher_id):
         try:
+            logger.info(f"{get_request_id()} Calling handler for approving teacher")
             TeacherHandler().approve_teacher(teacher_id)
+            logger.info(
+                f"{get_request_id()} formatting response after successfull approval"
+            )
             return SuccessResponse(
                 200, PromptMessage.APPROVE_SUCCESS.format("Teacher")
             ).get_json()
         except DataNotFound:
+            logger.info(f"{get_request_id()} formatting response for no teacher found")
             return abort(
                 404,
                 message=ErrorResponse(
@@ -54,6 +66,9 @@ class TeacherController:
                 ).get_json(),
             )
         except FailedAction:
+            logger.info(
+                f"{get_request_id()} Can't approve principal with id {teacher_id}"
+            )
             return abort(
                 409,
                 message=ErrorResponse(
@@ -63,11 +78,16 @@ class TeacherController:
 
     def delete_teacher(self, teacher_id):
         try:
+            logger.info(f"{get_request_id()} Calling handler for deleting teacher")
             TeacherHandler().delete_teacher(teacher_id)
+            logger.info(f"{get_request_id()} fromating response after deleting teacher")
             return SuccessResponse(
                 200, PromptMessage.SUCCESS_ACTION.format("Teacher deleted")
             ).get_json()
         except DataNotFound:
+            logger.info(
+                f"{get_request_id()} formatting response for no teacher found to delete with id {teacher_id}"
+            )
             return abort(
                 404,
                 message=ErrorResponse(
